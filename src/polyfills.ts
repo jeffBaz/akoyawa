5de9f0ad8024aa82c49c8973dd29fbe2e62ad22d
DIR\jeff.bazir <jeff.bazir@accenture.com>
2018-03-12 10:52:40 +0100

    animatgion poyfill

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -51,7 +51,11 @@ import 'core-js/es7/reflect';
  * Needed for: All but Chrome, Firefox and Opera. http://caniuse.com/#feat=web-animation
  **/
 // import 'web-animations-js';  // Run `npm install --save web-animations-js`.
-
+/**
+* Required to support Web Animations `@angular/platform-browser/animations`.
+* Needed for: All but Chrome, Firefox and Opera. http://caniuse.com/#feat=web-animation
+**/
+import 'web-animations-js';  // Run `npm install --save web-animations-js`.
 
 
 /***************************************************************************************************
