d20cffe5629fceda34269072d7a9a23cd2ff0afc
DIR\jeff.bazir <jeff.bazir@accenture.com>
2018-03-12 12:20:59 +0100

    animatgion poyfill

--- app.js
+++ app.js
@@ -3,10 +3,12 @@ const http = require('http');
 const path = require('path');
 const PORT = process.env.OPENSHIFT_NODEJS_PORT || process.env.PORT || 4200
 console.log(`Listening on ${ PORT }`)
+
 express()
   .use(express.static(path.join(__dirname, 'dist')))
   .set('views', path.join(__dirname, 'views'))
   .set('port', PORT)
+  .get('*', routes.index)
   .set('view engine', 'ejs')
   .get('/', (req, res) => res.render('dist/index'))
   .listen(PORT, '0.0.0.0', () => console.log(`Listening on ${ path } ${ PORT }`))
